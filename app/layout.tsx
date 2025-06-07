865958478f96bf17b03b63f3002e15fc2eaaef2b
Umidjon-mobile <umidjongafforov175@gmail.com>
2025-06-07 13:25:47 +0500

    trvel-blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,22 @@ export const metadata: Metadata = {
 		'Silk Road destinations',
 		'Samarkand travel guide',
 	],
+	openGraph: {
+		title: ' Uzbekistan Travel Blog - WanderWise ',
+		description:
+			'Discover breathtaking destinations, travel tips, itineraries, and personal stories from around the globe. WanderWise is your ultimate travel companion.',
+		url: 'https://wanderwise.vercel.app/',
+		siteName: 'WanderWise',
+		images: [
+			{
+				url: 'https://lp-cms-production.imgix.net/2023-07/GettyImages-1193462464.jpg?auto=format,compress&q=72&fit=crop',
+				width: 800,
+				height: 600,
+			},
+		],
+		locale: 'en_US',
+		type: 'website',
+	},
 }
 export default function RootLayout({
 	children,
